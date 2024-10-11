bbd3ee3d624c0fb00c45a8adb290307ba656a33d
Exil <exil@n0t3x1l.dev>
2024-10-11 14:04:17 +0100

    Add `zsh` and `oh-my-zsh` configuration

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -187,6 +187,24 @@ in
     };
     programs = {
       home-manager.enable = true;
+      zsh = {
+        enable = true;
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
+
+        shellAliases = {
+          ll = "ls -l";
+          update = "sudo nixos-rebuild switch";
+        };
+        history = {
+          size = 10000;
+        };
+        oh-my-zsh = {
+          enable = true;
+          plugins = [ "git" "thefuck" ];
+          theme = "robbyrussell";
+        };
+      };
     };
     xdg = {
       # mime.enable = true;
