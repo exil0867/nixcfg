c8e3d3a5ccfbf7a982ac46fb455dead962c1e861
Exil <exil@n0t3x1l.dev>
2024-10-13 18:59:54 +0100

    Declare Tailscale using an alternative way

--- hosts/d3skt0p/default.nix
+++ hosts/d3skt0p/default.nix
@@ -59,33 +59,16 @@ in
     secrets."tailscale/s3rv3r/key" = {};
   };
 
-  services.tailscale.enable = true;
-
-  systemd.services.tailscale-autoconnect = {
-    description = "Automatic connection to Tailscale";
-
-    # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscale.service" ];
-    wants = [ "network-pre.target" "tailscale.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up --login-server=192.168.1.5 --authkey $(cat /run/secrets/tailscale/s3rv3r/key)
-    '';
+  services.tailscale = {
+    enable = true;
+    authKeyParameters = {
+      preauthorized = true;
+      baseURL = "192.168.1.5";
+    };
+    extraSetFlags = [
+      "--login-server=192.168.1.5"
+    ];
+    authKeyFile = sops.secrets."tailscale/s3rv3r/key".path;
   };
   
   home-manager.users.${vars.user} = {
