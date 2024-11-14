b85a27b97849450e71293bd996daa228218289a5
Exil <exil@n0t3x1l.dev>
2024-11-14 16:25:40 +0100

    Fix "Update Git configuration"

--- hosts/d3skt0p/default.nix
+++ hosts/d3skt0p/default.nix
@@ -80,13 +80,9 @@ in
         };
       };
       git = {
+        enable = true;
         userEmail = "exil@n0t3x1l.dev";
         userName = "Exil";
-        extraConfig = {
-          user.name = "Exil";
-          user.email = "exil@n0t3x1l.dev";
-          init.defaultBranch = "master";
-        };
       };
     };
   };
