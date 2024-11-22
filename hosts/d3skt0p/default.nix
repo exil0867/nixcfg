046877afa6a48ca385f8cfe0ff5de5956915e8ad
Exil <exil@n0t3x1l.dev>
2024-11-22 16:56:14 +0100

    Revert "Disable Tailscale service"
    This reverts commit 659d5212b2caa1c088b9733b145218ca95bda89d.

--- hosts/d3skt0p/default.nix
+++ hosts/d3skt0p/default.nix
@@ -52,7 +52,12 @@ in
   };
 
 
-
+  services.tailscale = {
+    enable = true;
+    extraUpFlags = ["--login-server" "https://192.168.1.5:8181"];
+    authKeyFile = config.age.secrets.tailscale-preauth-d3skt0p.path;
+    openFirewall = true;
+  };
   
   home-manager.users.${vars.user} = {
     imports = [
