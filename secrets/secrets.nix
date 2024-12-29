b2914091352286cec77cc03d886fa5fad514a69f
Exil <exil@n0t3x1l.dev>
2024-12-29 23:31:24 +0100

    Test Agenix with the new host

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   personal_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItpAE9vRUSAOZAqG9rUmS58ANi/kIIdM9Ki34kEARIP";
-  remote_server_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMRBd09TZ22IUNl3oST8w2/imcenmjTd9To5RL8O4rDc";
+  remote_server_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInjI+XzPKAmRH/S/zpx4XVusY8W0IbG6cithnOZBZJo";
   keys = [personal_key remote_server_key];
 in {
-  "tailscale/preauth-kairos.age".publicKeys = [personal_key];
+  "tailscale/preauth-kairos.age".publicKeys = [personal_key remote_server_key];
 }
