45d4c309b90fc29f7b9e8900c5fada9e8cec0c0b
Exil <exil@n0t3x1l.dev>
2025-03-16 01:30:47 +0100

    Add PNPM configuration to the ZSH start script

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -26,6 +26,14 @@
         #emulate zsh -c "$(direnv hook zsh)"
 
         #eval "$(direnv hook zsh)"
+
+        # pnpm
+        export PNPM_HOME="~/.local/share/pnpm"
+        case ":$PATH:" in
+          *":$PNPM_HOME:"*) ;;
+          *) export PATH="$PNPM_HOME:$PATH" ;;
+        esac
+        # pnpm end
       '';
     };
   };
