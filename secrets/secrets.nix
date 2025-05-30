ab0ef3da0cfe2fbbc1afa6888deefa5475479cf9
Exil <exil@n0t3x1l.dev>
2025-05-30 01:31:37 +0100

    Update secrets definition

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   personal_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItpAE9vRUSAOZAqG9rUmS58ANi/kIIdM9Ki34kEARIP";
   echo_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInjI+XzPKAmRH/S/zpx4XVusY8W0IbG6cithnOZBZJo";
-  sky_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqlP73DfKLVHNBbff/Uz4GHx+yq7ONBfFLHRLu+AK8e";
+  sky_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCMp+7wjoK6KyPn4h9q15/eYv0E2c8f1Vs99/Tmiy09";
 in {
   "cloudflare/n0t3x1l.dev-DNS-RW.age".publicKeys = [echo_key sky_key];
   "tailscale/preauth-kairos.age".publicKeys = [personal_key echo_key];
