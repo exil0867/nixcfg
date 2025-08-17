68ecc280df363de13ce08204364d29f7574dd7a4
Exil <exil@n0t3x1l.dev>
2025-08-17 13:29:17 +0100

    Update Sky public key for Agenix secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   personal_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItpAE9vRUSAOZAqG9rUmS58ANi/kIIdM9Ki34kEARIP";
   echo_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInjI+XzPKAmRH/S/zpx4XVusY8W0IbG6cithnOZBZJo";
-  sky_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDCMp+7wjoK6KyPn4h9q15/eYv0E2c8f1Vs99/Tmiy09";
+  sky_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEriuOuF5+iFUxyOZx3adyDj2mUt5hmJN73y/14v0pL2";
 in {
   "cloudflare/n0t3x1l.dev-DNS-RW.age".publicKeys = [echo_key sky_key];
   "tailscale/preauth-kairos.age".publicKeys = [personal_key echo_key];
