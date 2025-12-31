c2120cc1cb8ce8610fde7d77e192c53c1f1a98dc
Exil <exil@kyrena.dev>
2025-12-31 03:25:33 +0100

    Fix permissions issues

--- modules/programs/htpc-downloader.nix
+++ modules/programs/htpc-downloader.nix
@@ -1,18 +1,12 @@
 { config, lib, pkgs, ... }:
-
 with lib;
-
 let
   cfg = config.programs.htpc-downloader;
-  
   rawScript = builtins.readFile ../scripts/htpc-downloader.sh;
-  
   wrapperScript = pkgs.writeScriptBin "htpc-downloader" ''
     #!${pkgs.bash}/bin/bash
-    
     # Inject the variable from the module configuration
     export HTPC_ROOT="${cfg.mediaDir}"
-    
     ${rawScript}
   '';
 in
@@ -20,21 +14,20 @@ in
   # Define standard options for this module
   options.programs.htpc-downloader = {
     enable = mkEnableOption "HTPC Downloader Script";
-    
     mediaDir = mkOption {
       type = types.str;
       description = "The root path where media should be downloaded";
       example = "/mnt/data/downbox";
     };
-
     user = mkOption {
       type = types.str;
-      description = "The user that needs permissions for these folders";
-      default = "root";
+      description = "Primary owner of the HTPC media files";
     };
   };
 
   config = mkIf cfg.enable {
+    users.users.${cfg.user}.extraGroups = [ "transmission" ];
+    
     environment.systemPackages = [ 
       wrapperScript 
       pkgs.transmission_4 
@@ -42,9 +35,14 @@ in
 
     # Automatically create folders and set permissions
     systemd.tmpfiles.rules = [
-      "d ${cfg.mediaDir}/htpc/tv_shows 0775 ${cfg.user} transmission -"
-      "d ${cfg.mediaDir}/htpc/movies   0775 ${cfg.user} transmission -"
-      "d ${cfg.mediaDir}/htpc/other    0775 ${cfg.user} transmission -"
+      # Create root directories with setgid and write permissions for group
+      "d ${cfg.mediaDir}/htpc 2775 ${cfg.user} transmission -"
+      "d ${cfg.mediaDir}/htpc/tv_shows 2775 ${cfg.user} transmission -"
+      "d ${cfg.mediaDir}/htpc/movies 2775 ${cfg.user} transmission -"
+      "d ${cfg.mediaDir}/htpc/other 2775 ${cfg.user} transmission -"
+      
+      # Recursively fix permissions on ALL existing content
+      "Z ${cfg.mediaDir}/htpc 2775 ${cfg.user} transmission -"
     ];
   };
 }
