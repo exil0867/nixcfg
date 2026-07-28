2db996a7ff68401490801c62470b8dcab66f132c
Exil <exil@kyrena.dev>
2026-07-29 00:14:50 +0100

    Disable the resources limitation when rebuilding.

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -191,8 +191,8 @@ in {
   nix = {
     settings = {
       auto-optimise-store = true;
-      max-jobs = 1;
-      cores = 2;
+      # max-jobs = 1;
+      # cores = 2;
     };
     gc = {
       automatic = true;
